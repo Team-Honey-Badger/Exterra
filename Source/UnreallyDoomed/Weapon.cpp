d559e542b45bf2d4e57c3915a7136f43fede4a2a
Noujou <gbencivengo4@gmail.com>
2015-04-22 18:30:14 -0400

    added some comments to weapon.cpp

--- Source/UnreallyDoomed/Weapon.cpp
+++ Source/UnreallyDoomed/Weapon.cpp
@@ -19,12 +19,12 @@ AWeapon::AWeapon()
 	WeaponMesh->AttachTo(RootComponent);
 }
 
+//method that is called whenever the fire button is pressed
 void AWeapon::Fire()
 {
 	if (ProjectileType == EWeaponProjectile::eBullet) {
 		if (CurrentClip > 0)
 		{
-			GEngine->AddOnScreenDebugMessage(-1, 2.f, FColor::Black, TEXT("Bullet"));
 			InstantFire();
 			PlayWeaponSound(FireSound);
 			CurrentClip -= WeaponConfig.shotCost;
@@ -42,7 +42,6 @@ void AWeapon::Fire()
 	if (ProjectileType == EWeaponProjectile::eSpread) {
 		if (CurrentClip > 0)
 		{
-			GEngine->AddOnScreenDebugMessage(-1, 2.f, FColor::Black, TEXT("Spread"));
 			for (int32 counter = 0; counter <= WeaponConfig.WeaponSpread; counter++) {
 				InstantFire();
 			}
@@ -62,7 +61,6 @@ void AWeapon::Fire()
 	if (ProjectileType == EWeaponProjectile::eProjectile) {
 		if (CurrentClip > 0)
 		{
-			GEngine->AddOnScreenDebugMessage(-1, 2.f, FColor::Black, TEXT("Projectile"));
 			ProjectileFire();
 			PlayWeaponSound(FireSound);
 			CurrentClip -= WeaponConfig.shotCost;
@@ -79,6 +77,7 @@ void AWeapon::Fire()
 	}
 }
 
+//sets the owner of the weapon to the player
 void AWeapon::SetOwningPawn(AFPSCharacter *NewOwner)
 {
 	if (MyPawn != NewOwner)
@@ -88,17 +87,21 @@ void AWeapon::SetOwningPawn(AFPSCharacter *NewOwner)
 	}
 }
 
+//method will attach the weapon to the player when called
 void AWeapon::OnEquip()
 {
 	CollisionComp->SetCollisionEnabled(ECollisionEnabled::NoCollision);
 	AttachToPlayer();
 }
 
+//detaches the weapon from the player
 void AWeapon::OnUnEquip()
 {
 	DetachFromPlayer();
 }
 
+//method that checks to see if the player exists and if it does then it will attach the weapon to the player
+//if there is a weapon currently attached, then detach it, for the new weapon.
 void AWeapon::AttachToPlayer()
 {
 	if (MyPawn)
@@ -111,12 +114,19 @@ void AWeapon::AttachToPlayer()
 	}
 }
 
+//detaches the weapon from the character
 void AWeapon::DetachFromPlayer()
 {
 	WeaponMesh->DetachFromParent();
 	WeaponMesh->SetHiddenInGame(true);
 }
 
+/*
+Method to reload ammo.
+How it works:
+if the current clip is 0, then subtract the max clip from the current ammo and put it in the current clip
+if the current ammo is 0, then you are out of ammo
+*/
 void AWeapon::ReloadAmmo()
 {
 	if (CurrentAmmo > 0)
@@ -137,6 +147,7 @@ void AWeapon::ReloadAmmo()
 	}
 }
 
+//the instant, ray trace firing system, used for all non-projectile weapons.
 void AWeapon::InstantFire()
 {
 
@@ -159,6 +170,7 @@ void AWeapon::ProjectileFire()
 	//calls the one in Rocket
 }
 
+//creates the ray trace for the instant fire
 FHitResult AWeapon::WeaponTrace(const FVector &TraceFrom, const FVector &TraceTo) const
 {
 	static FName WeaponFireTag = FName(TEXT("WeaponTrace"));
@@ -175,19 +187,13 @@ FHitResult AWeapon::WeaponTrace(const FVector &TraceFrom, const FVector &TraceTo
 	return Hit;
 }
 
+//checks to see if you hit anything, if it's a actor, then deal damage
 void AWeapon::ProcessInstantHit(const FHitResult &Impact, const FVector &Origin, const FVector &ShootDir, int32 RandomSeed, float ReticleSpeed)
 {
 	const FVector EndTrace = Origin + ShootDir * WeaponConfig.WeaponRange;
 	const FVector EndPoint = Impact.GetActor() ? Impact.ImpactPoint : EndTrace;
 
 	DrawDebugLine(this->GetWorld(), Origin, Impact.TraceEnd, FColor::Black, true, 1.f, 3.f);
-
-	AEnemy *Enemy = Cast<AEnemy>(Impact.GetActor());
-	if (Enemy)
-	{
-		GEngine->AddOnScreenDebugMessage(-1, 3.f, FColor::Red, "YOU HIT AN ENEMY!!");
-		Enemy->Destroy();
-	}
 
 	// Deal damage to the hit actor
 	if (Impact.GetActor()){
